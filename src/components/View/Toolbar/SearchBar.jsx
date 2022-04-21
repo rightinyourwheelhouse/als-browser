c5d4470908cd3e97525f9b964162244304cea780
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-04-21 16:33:49 +0200

    feat(toolbar): Toolbar is now working, still some WIP

--- src/components/View/Toolbar/SearchBar.jsx
+++ src/components/View/Toolbar/SearchBar.jsx
@@ -5,7 +5,7 @@ import OnType from '../Dashboard/OnType';
 
 const SearchBar = ({ onFocusChange }) => {
 	const [input, setInput] = useState('');
-	const [currentURL, setCurrentURL] = useState();
+	// const [currentURL, setCurrentURL] = useState();
 
 	const inputRef = useRef();
 
@@ -28,13 +28,13 @@ const SearchBar = ({ onFocusChange }) => {
 		}
 	};
 
-	window.api.recieve('loadURLResponse', (url) => {
-		if (url.length <= 0) {
-			setCurrentURL('Typ een website om te zoeken');
-		} else {
-			setCurrentURL(url[0]);
-		}
-	});
+	// window.api.recieve('loadURLResponse', (url) => {
+	// 	if (url.length <= 0) {
+	// 		setCurrentURL('Typ een website om te zoeken');
+	// 	} else {
+	// 		setCurrentURL(url[0]);
+	// 	}
+	// });
 
 	return (
 		<form onSubmit={handleSubmit}>
@@ -42,7 +42,6 @@ const SearchBar = ({ onFocusChange }) => {
 				ref={inputRef}
 				className="hover:drop-shadow-browser h-14 w-96 rounded-full pl-4 text-center drop-shadow-light transition duration-300 ease-in-out placeholder:text-center"
 				onInput={(e) => handleOnChange(e)}
-				value={currentURL}
 				onFocus={() => handleOnFocus(true)}
 				onBlur={() => handleOnFocus(false)}
 				placeholder="Typ een website om te zoeken"
