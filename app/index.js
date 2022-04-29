1166e100f5a4698c33c6e9a4995bcdb47712f166
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-04-29 08:19:31 +0200

    fix(searchbar): Always using HTTPS for the safety of our users.

--- app/index.js
+++ app/index.js
@@ -98,22 +98,21 @@ ipcMain.on('goForward', () => {
 });
 
 ipcMain.on('searchURL', (event, url) => {
+	// eslint-disable-next-line no-useless-escape
 	const exp =
-		// eslint-disable-next-line no-useless-escape
-		/((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
+		/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
+
 	const regex = new RegExp(exp);
 
 	if (regex.test(url)) {
-		if (!url.includes('www')) {
-			url = 'www.' + url;
-		}
-		if (!url.includes('http://')) {
-			url = 'http://' + url;
-		}
+		if (url.includes('www')) url = 'https://' + url;
+		if (!url.includes('www')) url = 'https://www.' + url;
+		view.webContents.loadURL(url);
 	} else {
-		url = 'http://www.google.com/search?q=' + url;
+		const newUrl = 'https://www.google.com/search?q=' + url;
+		view.webContents.loadURL(newUrl);
+		console.log(url, 'IS NOT VALID');
 	}
-	view.webContents.loadURL(url);
 
 	// When dashboard is loaded, set the browserView back
 	if (view.getBounds().width === 0 && view.getBounds().height === 0)
