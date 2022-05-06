0db2cc662b671203ca5d441a89b778e3d708c76d
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-05-06 10:51:26 +0200

    feat: Added user feedback for loading screen

--- app/index.js
+++ app/index.js
@@ -8,13 +8,9 @@ const isDev = require('electron-is-dev');
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) app.quit();
 
-
+let loadingScreen;
 let mainWindow;
 let view;
-
-function sendStatusToWindow(text) {
-	mainWindow.webContents.send('message', text);
-}
 
 function createWindow() {
 	// Always set the theme to light
@@ -56,28 +52,6 @@ function createWindow() {
 	mainWindow.once('ready-to-show', () => {
 		autoUpdater.checkForUpdatesAndNotify();
 	});
-
-	autoUpdater.on('checking-for-update', () => {
-		sendStatusToWindow('Checking for update...');
-	});
-	autoUpdater.on('update-available', (info) => {
-		sendStatusToWindow('Update available.');
-	});
-	autoUpdater.on('update-not-available', (info) => {
-		sendStatusToWindow('Update not available.');
-	});
-	autoUpdater.on('error', (err) => {
-		sendStatusToWindow('Error in auto-updater. ' + err);
-	});
-	autoUpdater.on('download-progress', (progressObj) => {
-		let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
-		log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-		log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
-		sendStatusToWindow(log_message);
-	});
-	autoUpdater.on('update-downloaded', (info) => {
-		sendStatusToWindow('Update downloaded');
-	});
 }
 
 function createBrowserView() {
@@ -103,16 +77,62 @@ function createBrowserView() {
 }
 
 app.whenReady().then(() => {
-	createWindow();
 	app.on('activate', function () {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
-	app.on('ready', function () {
-		autoUpdater.checkForUpdatesAndNotify();
-	});
+
+	createLoadingScreen();
+	autoUpdater.checkForUpdatesAndNotify();
+	setTimeout(() => {
+		createWindow();
+	}, 20000);
 });
+
+function sendLoadingStatusToWindow(text) {
+	loadingScreen.webContents.send('message', text);
+}
+
+const createLoadingScreen = () => {
+	loadingScreen = new BrowserWindow({
+		width: 900,
+		height: 700,
+		frame: false,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	});
+	loadingScreen.webContents.openDevTools();
+	loadingScreen.setResizable(false);
+	loadingScreen.loadURL('file://' + __dirname + '/loading/loading.html');
+	loadingScreen.on('closed', () => (loadingScreen = null));
+	loadingScreen.webContents.on('did-finish-load', () => {
+		loadingScreen.show();
+	});
+
+	autoUpdater.on('checking-for-update', () => {
+		sendLoadingStatusToWindow('Checking for update...');
+	});
+	autoUpdater.on('update-available', () => {
+		sendLoadingStatusToWindow('Update available.');
+	});
+	autoUpdater.on('update-not-available', () => {
+		sendLoadingStatusToWindow('Update not available.');
+	});
+	autoUpdater.on('error', (err) => {
+		sendLoadingStatusToWindow('Error in auto-updater. ' + err);
+	});
+	autoUpdater.on('download-progress', (progressObj) => {
+		let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+		log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+		log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+		sendLoadingStatusToWindow(log_message);
+	});
+	autoUpdater.on('update-downloaded', () => {
+		sendLoadingStatusToWindow('Update downloaded');
+	});
+};
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
