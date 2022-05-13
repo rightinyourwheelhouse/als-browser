507f89eadbaa9b26fc281fb99d9a16783afda20a
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-05-13 12:06:56 +0200

    removed console.log

--- src/contexts/ExtensionStatesContextProvider.jsx
+++ src/contexts/ExtensionStatesContextProvider.jsx
@@ -33,7 +33,6 @@ const ExtensionStatesContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		window.api.recieve('setLatestOverlayLocationReply', (payload) => {
-			console.log(payload);
 			setExtensionStates((previousState) => ({
 				...previousState,
 				scrollHelpPosition: { top: payload[0].top, left: payload[0].left },
