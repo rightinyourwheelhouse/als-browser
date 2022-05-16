5b97c87025fdd2524009dd2741b22b662946ce47
Tommy Tytgat <tommy@wheelhouse.be>
2022-05-16 14:58:37 +0200

    fix(feedback): excluded (seamingly) useless script

--- src/components/Settings/components/Pages/FeedbackSetting.jsx
+++ src/components/Settings/components/Pages/FeedbackSetting.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
 const FeedbackSetting = () => {
-
 	return (
-		<div>
-			<div className="h-[calc(100vh-5em)]">
-				<iframe
-					className="clickup-embed clickup-dynamic-height"
-					src="https://forms.clickup.com/24323508/f/q69dm-29381/B9D37296WIKIQ99ME2"
-					width="100%"
-					height="100%"
-				></iframe>
-				<script async src="https://app-cdn.clickup.com/assets/js/forms-embed/v1.js"></script>
-			</div>
+		<div className="h-[calc(100vh-5rem)]">
+			<iframe
+				className="clickup-embed clickup-dynamic-height"
+				src="https://forms.clickup.com/24323508/f/q69dm-29381/B9D37296WIKIQ99ME2"
+				width="100%"
+				height="100%"
+			></iframe>
 		</div>
 	);
 };
