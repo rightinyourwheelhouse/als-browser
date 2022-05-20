2b324fa8bf6e2fb19ecaae30f9bad9d0935fbe93
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-05-20 10:04:19 +0200

    fix: bigTile image is now a component

--- src/components/Dashboard/Tiles/BigTile.jsx
+++ src/components/Dashboard/Tiles/BigTile.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
+import TileImage from './TileImage';
 
 const BigTile = ({ title, img, description, url }) => {
-	const [fallback, setFallback] = useState(false);
 	const handleChangeUrl = () => {
 		window.api.send('searchURL', url);
 	};
@@ -13,17 +13,7 @@ const BigTile = ({ title, img, description, url }) => {
 		>
 			<div className="flex flex-row gap-8">
 				<div className="flex items-center justify-start gap-4">
-					<div>
-						{img && !fallback ? (
-							<img onError={() => setFallback(true)} className="h-7 w-7 bg-white" src={img} />
-						) : (
-							<div>
-								<span className="flex h-10 w-10 items-center justify-center rounded-md bg-slate-500 text-xl font-bold text-white">
-									{title?.substring(0, 1)}
-								</span>
-							</div>
-						)}
-					</div>
+					<TileImage title={title} src={img} />
 					<div className="text-left">
 						<a className="text-sm font-light underline underline-offset-1">{url}</a>
 						<h2 className="text-lg font-semibold">{title}</h2>
