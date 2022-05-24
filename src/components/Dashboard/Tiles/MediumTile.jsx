3b1ab1d5df24c70c22622f38989e4dc22c681d7a
EwoutVer <43569270+EwoutVerhamme@users.noreply.github.com>
2022-05-24 13:23:05 +0200

    fix: added fallback for image in suggestion

--- src/components/Dashboard/Tiles/MediumTile.jsx
+++ src/components/Dashboard/Tiles/MediumTile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TileImage from './TileImage';
 
 const MediumTile = ({ title, img, url }) => {
 	const handleChangeUrl = () => {
@@ -9,9 +10,11 @@ const MediumTile = ({ title, img, url }) => {
 			onClick={handleChangeUrl}
 			className="mb-4 flex h-20 w-60 min-w-max items-center justify-start gap-4 rounded-2xl bg-white pr-4 drop-shadow-light transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-hover"
 		>
-			<img className="ml-4 h-12 w-12 bg-gray-50" src={img} alt={`${title}'s favicon`} />
+			<div className="ml-4">
+				<TileImage size="h-12 w-12" src={img} title={title} alt={`${title}'s favicon`} />
+			</div>
+
 			<h2 className="font-mulish text-base font-medium">
-			
 				{title.length >= 24 ? `${title.substring(0, 24)} ...` : title}
 			</h2>
 		</button>
