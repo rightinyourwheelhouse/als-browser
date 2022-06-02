1f28229ad803b4d3714cd09f24e50428bac7aa2e
RobrechtUlenaersPXL <72489597+RobrechtUlenaersPXL@users.noreply.github.com>
2022-06-02 12:26:31 +0200

    fixed gravitywell ofsset + delted unnessacary files

--- app/scripts/gravityWells.js
+++ app/scripts/gravityWells.js
@@ -323,7 +323,8 @@ async function makeGravityWell() {
 	let distance = Math.sqrt(Math.pow(clientX - elementPosMiddle[0], 2) + Math.pow(clientY - elementPosMiddle[1], 2));
 	let target_X = undefined;
 	let target_Y = undefined;
-	let heightOffset = window.outerHeight - window.innerHeight + 20;
+	let heightOffset = window.outerHeight - window.innerHeight + 16;
+	console.log(heightOffset)
 	let diffX = Math.min(10, 0 + 1 * (Math.abs(elementPosMiddle[0] - clientX) / 100));
 	let diffY = Math.min(10, 0 + 1 * (Math.abs(elementPosMiddle[1] - clientY) / 100));
 	if (distance < 100) {
